4b69edb1f98417935d000af6f983ddaaf09bd9a3
Luis Felce <luisr.felce@gmail.com>
2021-06-27 17:16:03 +0200

    final

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//graficos
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { PieComponent } from './components/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent,
+    PieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
